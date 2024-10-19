69514b8bcc6af55910cfdb081d50e058b53bb82f
TOConde <tomy_t.c@hotmail.com>
2024-10-19 10:26:33 -0300

    agregado basico de infotabs

--- boapp/app/components/banner/Banner.tsx
+++ boapp/app/components/banner/Banner.tsx
@@ -1,10 +1,13 @@
 import './Banner.css'
+import { InfoTabs } from './infoTabs/InfoTabs';
+import { LanguageBttn } from './languageBttn/LanguageBttn';
 
 export const Banner = () => {
   return (
     <div className='banner'>
       <img className='logoBanner' src="./img/BIST.png"/>
-      <button>lenguaje</button>
+      <InfoTabs />
+      <LanguageBttn />
     </div>
   );
 }
