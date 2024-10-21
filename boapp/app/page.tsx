e61c3b847dfbf6c6e9d952cd466324d09c278d18
TOConde <tomy_t.c@hotmail.com>
2024-10-21 10:04:05 -0300

    base de i18n

--- boapp/app/page.tsx
+++ boapp/app/page.tsx
@@ -3,6 +3,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Banner } from "./components/banner/Banner";
 import styles from "./page.module.css";
+import './i18n';
 
 export default function Home() {
   return (
