ced15b77256a6d3da34037edf5fc5fe4ae7162b4
TOConde <tomy_t.c@hotmail.com>
2024-10-25 14:59:07 -0300

    base para usar gugl chart

--- boapp/app/page.tsx
+++ boapp/app/page.tsx
@@ -4,11 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Banner } from "./components/banner/Banner";
 import styles from "./page.module.css";
 import './i18n';
+import { LineChart } from './components/navBar/chart/Chart';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Banner />
+      <LineChart />
     </main>
   );
 }
