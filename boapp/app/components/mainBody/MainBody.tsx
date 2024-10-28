6e3a678a92144017d101b6b76c67b24fd56f58c5
TOConde <tomy_t.c@hotmail.com>
2024-10-28 16:53:02 -0300

    layout del chart y navbar

--- boapp/app/components/mainBody/MainBody.tsx
+++ boapp/app/components/mainBody/MainBody.tsx
@@ -1,10 +1,12 @@
 import { LineChart } from './chart/Chart';
 import './MainBody.css'
+import { NavBar } from './navBar/NavBar';
 
 export const MainBody = () => {
   return (
-    <div className=''>
+    <div className='mainContainer'>
       <LineChart />
+      <NavBar />
     </div>
   );
 }
