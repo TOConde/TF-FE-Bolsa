253fcf86853b5477b4c29d2b69d095a5726a2c62
TOConde <tomy_t.c@hotmail.com>
2024-10-28 15:01:58 -0300

    reoganizacion de archivo chart

---
+++ boapp/app/components/mainBody/MainBody.tsx
@@ -0,0 +1,10 @@
+import { LineChart } from './chart/Chart';
+import './MainBody.css'
+
+export const MainBody = () => {
+  return (
+    <div className=''>
+      <LineChart />
+    </div>
+  );
+}
--- boapp/app/page.tsx
+++ boapp/app/page.tsx
@@ -4,13 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Banner } from "./components/banner/Banner";
 import styles from "./page.module.css";
 import './i18n';
-import { LineChart } from './components/navBar/chart/Chart';
+import { MainBody } from './components/mainBody/MainBody';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Banner />
-      <LineChart />
+      <MainBody />
     </main>
   );
 }
