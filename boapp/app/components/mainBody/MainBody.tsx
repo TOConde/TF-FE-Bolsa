acdf169f96645a27c0b71b893aec7592b7d71a00
TOConde <tomy_t.c@hotmail.com>
2024-10-31 09:07:18 -0300

    imagen bolsa redirecciona a top

--- boapp/app/components/mainBody/MainBody.tsx
+++ boapp/app/components/mainBody/MainBody.tsx
@@ -4,7 +4,7 @@ import { NavBar } from './navBar/NavBar';
 
 export const MainBody = () => {
   return (
-    <div className='mainContainer'>
+    <div id='home' className='mainContainer'>
       <LineChart />
       <NavBar />
     </div>
