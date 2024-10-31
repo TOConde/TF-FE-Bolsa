58eaf6b79356571d36da888bd0c028233266449b
TOConde <tomy_t.c@hotmail.com>
2024-10-31 09:59:06 -0300

    agregado footer basico

--- boapp/app/page.tsx
+++ boapp/app/page.tsx
@@ -6,6 +6,7 @@ import styles from "./page.module.css";
 import './i18n';
 import { MainBody } from './components/mainBody/MainBody';
 import AboutUs from './components/aboutUs/AboutUs';
+import Footer from './components/footer/Footer';
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Banner />
       <MainBody />
       <AboutUs />
+      <Footer />
     </main>
   );
 }
