e28021f0bc1a6cf303606bbc43976be9bcad7e60
TOConde <tomy_t.c@hotmail.com>
2024-11-24 16:11:06 -0300

    translation en buttons

--- boapp/app/components/mainBody/sideBar/buttonsSide/ButtonsSide.tsx
+++ boapp/app/components/mainBody/sideBar/buttonsSide/ButtonsSide.tsx
@@ -1,15 +1,17 @@
 import { Button } from 'react-bootstrap';
 import './ButtonsSide.css'
+import { useTranslation } from 'next-i18next';
 
 export default function ButtonsSide() {
+  const { t } = useTranslation();
 
   return (
     <div className='containerButtonsSide'>
       <Button variant="primary" size="sm">
-        Empresas
+        {t('sideBar.buttons.empresas')}
       </Button>
       <Button variant="primary" size="sm">
-        Indices
+        {t('sideBar.buttons.indices')}
       </Button>
     </div>
   );
