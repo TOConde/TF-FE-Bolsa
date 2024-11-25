7946ae4a99fffeae44b3966c53a85d8e790a324f
TOConde <tomy_t.c@hotmail.com>
2024-11-25 19:55:22 -0300

    grafico de velas con data del back

--- boapp/app/components/mainBody/MainBody.tsx
+++ boapp/app/components/mainBody/MainBody.tsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from 'react';
-import { data, LineChart } from './chart/Chart';
+import { CanddleChart } from './chart/Chart';
 import './MainBody.css'
 import SideBar from './sideBar/SideBar';
-import { getAllEmpresas } from '@/app/services/Empresa';
+import { getAllEmpresas, getCotizacionesEmpresa } from '@/app/services/Empresa';
 import { Empresa } from '@/app/types/empresa';
+import { Cotizacion } from '@/app/types/cotizaciones';
 
 export const MainBody = () => {
   const [selectedEmpresa, setSelectedEmpresa] = useState<Empresa | null>(null);
   const [empresas, setEmpresas] = useState<Empresa[]>([]);
+  const [chartData, setChartData] = useState<Array<any>>([]);
 
   useEffect(() => {
     const fetchEmpresas = async () => {
@@ -25,6 +27,35 @@ export const MainBody = () => {
     fetchEmpresas();
   }, []);
 
+  useEffect(() => {
+    const fetchCotizaciones = async () =>{
+      if (selectedEmpresa) {
+        try {
+          const fechaDesde = '2024-10-01';
+          const fechaHasta = '2024-10-07';
+          const escala = 'mes';
+          const cotizaciones = await getCotizacionesEmpresa(selectedEmpresa.codEmpresa, fechaDesde, fechaHasta, escala);
+
+          const formattedData = [
+            ["Label", "Min-Max, Open-Close", "", "", ""],
+            ...cotizaciones.map((c: Cotizacion) => [
+              c.fecha,
+              parseFloat(c.minimo.toString()), 
+              parseFloat(c.apertura.toString()), 
+              parseFloat(c.cierre.toString()), 
+              parseFloat(c.maximo.toString()),
+            ])
+          ]
+          setChartData(formattedData);
+        } catch (error) {
+          console.error('Error fetching cotizaciones:', error);
+        }
+      }
+    }
+
+    fetchCotizaciones();
+  }, [selectedEmpresa])
+
   const handleSelectEmpresa = (id: string) => {
     const empresa = empresas.find((e) => e.id === id);
     setSelectedEmpresa(empresa || null);
@@ -33,7 +64,7 @@ export const MainBody = () => {
   return (
     <div id='home' className='mainContainer'>
       <div className='containerGrafico'>
-        <LineChart /* selectedEmpresa={selectedEmpresa} */ />
+        <CanddleChart chartData={chartData} />
       </div>
       <SideBar empresas={empresas} onSelectEmpresa={handleSelectEmpresa} selectedEmpresa={selectedEmpresa} />
     </div>
