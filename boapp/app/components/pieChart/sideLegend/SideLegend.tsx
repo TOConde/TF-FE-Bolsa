2a22e9d8818ce49f5e9e1110bc407eee5154e37c
TOConde <tomy_t.c@hotmail.com>
2024-12-01 16:50:59 -0300

    traducciones

--- boapp/app/components/pieChart/sideLegend/SideLegend.tsx
+++ boapp/app/components/pieChart/sideLegend/SideLegend.tsx
@@ -1,9 +1,12 @@
+import { useTranslation } from 'next-i18next';
 import './SideLegend.css'
 
 export default function SideLegend({ data }: { data: (string | number)[][] }) {
+  const { t } = useTranslation();
+  
   return (
     <div className='containerSideLegend'>
-      <h3>Detalle de Empresas</h3>
+      <h3>{t('piechart.sideBarTitulo')}</h3>
       <ul>
         {data.slice(1).map(([nombre, valorTotal]) => (
           <li key={nombre as string}>
